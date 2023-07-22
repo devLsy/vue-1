d3ea1e8303e7b2f5df816892e075b2fa6ff57cf2
devlsy86 <infightersy@naver.com>
2023-07-22 16:32:44 +0900

    반복문으로 수저

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div class="menu">
+    <a v-for="item in menus" :key="item">{{ item}}</a>
+  </div>
+
+  <div v-for="(item, i) in products" :key="item">
+    <h4>{{ products[i]}} 원룸</h4>
+    <p>60만원</p>
+  </div>    
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      menus: ['Home', 'Shop', 'About'],
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
@@ -21,6 +34,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
 </style>
